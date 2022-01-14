7e24e239a707d178137393ce0d83b6d7f0f89457
Marco Salvi <marco.salvi@gmail.com>
2022-01-14 10:45:26 +0100

    Adding DynamoDB table for storing historical

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 import json
 import datetime
-
+import cbpro
+import os
 
 def handler(event, context):
+
+    c = cbpro.PublicClient()
+    [(date, open_price, max_price, min_price, close_price, capitalization)] = c.get_product_historic_rates(product_id='BTC-EUR', start="2022-01-10T12:00:00",end="2022-01-10T12:10:00",granularity=3600)
+
     data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'BTC_EUR' : close_price,
+        'TABLE_NAME' : os.environ['TABLE_NAME']
     }
+
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
